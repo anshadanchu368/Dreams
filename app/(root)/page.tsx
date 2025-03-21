82c30b75e968cf556b38f186899af0d9008b07a2
Hashim Anhad <anshadanchu368@gmail.com>
2025-03-22 03:13:17 +0530

    head section has been set

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,7 +2,12 @@
 export default function Home() {
   return (
    <>
+   <section className="flex flex-col items-center bg-pink-100">
     <h1 className="heading">Home</h1>
+
+    <p className="text-black-100 text-md !max-w-3xl font-sans my-2">Pitch Your Ideas And Get funded By India's Top investors</p>
+
+   </section>
    </>
   );
 }
